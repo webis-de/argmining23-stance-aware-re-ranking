59e917a49755eb819800bb1fbecce0a30ee9e6fe
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-06-27 12:20:20 +0200

    Add NLTK utils

---
+++ fare/utils/nltk.py
@@ -0,0 +1,30 @@
+from nltk.downloader import Downloader
+from requests import head
+from requests.exceptions import ConnectionError
+
+from fare.logging import logger
+
+SKIPPED_NLTK_DOWNLOAD = False
+
+
+def download_nltk_dependencies(*dependencies: str):
+    global SKIPPED_NLTK_DOWNLOAD
+    if SKIPPED_NLTK_DOWNLOAD:
+        return
+
+    try:
+        head(Downloader.DEFAULT_URL, timeout=1)
+    except ConnectionError:
+        if not SKIPPED_NLTK_DOWNLOAD:
+            SKIPPED_NLTK_DOWNLOAD = True
+            logger.warning(
+                "Could not connect to NLTK servers. "
+                "Skipping NLTK download."
+            )
+        return
+
+    downloader = Downloader()
+    for dependency in dependencies:
+        if not downloader.is_installed(dependency):
+            logger.info(f"Downloading NLTK dependency {dependency}.")
+            downloader.download(dependency)
