37fa825cf338f2a79e0d82109354f483950c2702
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-10-09 14:42:47 +0200

    Add tie breaking

--- fare/metric/fairness.py
+++ fare/metric/fairness.py
@@ -8,7 +8,9 @@ from dataclasses import dataclass, field
 from enum import Enum
 from functools import cached_property
 from math import log
-from typing import Iterable, Union, Iterator, Hashable, Optional, Final
+from random import choice
+from typing import Iterable, Union, Iterator, Hashable, Optional, Final, \
+    Sequence
 
 from ir_measures import DefaultPipeline
 from ir_measures.measures import Measure, ParamInfo
@@ -22,9 +24,15 @@ Qrels = Union[Iterable[Qrel], dict[str, dict[str, int]], DataFrame]
 Run = Union[Iterable[ScoredDoc], dict[str, dict[str, int]], DataFrame]
 
 
-class ProtectedGroupSelection(Enum):
+class ProtectedGroupStrategy(Enum):
     MINORITY = "minority"
     MAJORITY = "majority"
+
+
+class TieBreakingStrategy(Enum):
+    RANDOM = "random"
+    GROUP_ASCENDING = "group-ascending"
+    GROUP_DESCENDING = "group-descending"
 
 
 class FairnessMeasure(Measure, ABC):
@@ -36,6 +44,7 @@ class FairnessMeasure(Measure, ABC):
         ),
         "group_col": ParamInfo(
             dtype=Hashable,
+            required=False,
             default="group",
             desc="group column in run"
         ),
@@ -46,8 +55,17 @@ class FairnessMeasure(Measure, ABC):
         ),
         "protected_group": ParamInfo(
             dtype=Hashable,
-            default=ProtectedGroupSelection.MINORITY,
+            required=False,
+            default=ProtectedGroupStrategy.MINORITY.value,
             desc="protected group name or selection strategy"
+        ),
+        "tie_breaking": ParamInfo(
+            dtype=Hashable,
+            required=False,
+            desc="tie breaking strategy when selecting the protected group "
+                 "using the minority or majority strategies or a "
+                 "comma-separated preference list"
+                 "(if not specified, ties will raise an exception)"
         ),
     }
 
@@ -71,14 +89,38 @@ class FairnessMeasure(Measure, ABC):
 
     @cached_property
     def _protected_group_param(self) -> Union[
-        ProtectedGroupSelection, Hashable
+        ProtectedGroupStrategy, Hashable
     ]:
         if "protected_group" not in self.params:
-            return ProtectedGroupSelection.MINORITY
+            return ProtectedGroupStrategy.MINORITY
         protected_group = self.params["protected_group"]
-        if protected_group in {s.value for s in ProtectedGroupSelection}:
-            return ProtectedGroupSelection(protected_group)
+        if protected_group in {s.value for s in ProtectedGroupStrategy}:
+            return ProtectedGroupStrategy(protected_group)
+        if not isinstance(protected_group, Hashable):
+            raise ValueError(
+                f"Illegal protected_group param: {protected_group}"
+            )
         return protected_group
+
+    @cached_property
+    def _tie_breaking_param(self) -> Union[
+        TieBreakingStrategy, Sequence, None
+    ]:
+        if "tie_breaking" not in self.params:
+            return None
+        tie_breaking = self.params["tie_breaking"]
+        if tie_breaking is None:
+            return None
+        if tie_breaking in {s.value for s in TieBreakingStrategy}:
+            return TieBreakingStrategy(tie_breaking)
+        if not isinstance(tie_breaking, Hashable):
+            raise ValueError(
+                f"Illegal tie_breaking param: {tie_breaking}"
+            )
+        return [
+            group.strip()
+            for group in str(tie_breaking).split(",")
+        ]
 
     @abstractmethod
     def fairness(
@@ -90,13 +132,82 @@ class FairnessMeasure(Measure, ABC):
     ) -> float:
         pass
 
+    def _group_counts(
+            self,
+            qrels: DataFrame,
+    ) -> dict[Hashable, int]:
+        counts = qrels.groupby(self._group_col_param).size().to_dict()
+        counts = defaultdict(lambda: 0, counts)
+        return counts
+
+    def _protected_group(self, group_counts: dict[Hashable, int]) -> Hashable:
+        protected_group: Hashable = self._protected_group_param
+        if isinstance(protected_group, ProtectedGroupStrategy):
+            strategy: ProtectedGroupStrategy = protected_group
+            groups: list[tuple[Hashable, int]] = [
+                item for item in group_counts.items()
+            ]
+            if strategy == ProtectedGroupStrategy.MINORITY:
+                groups = sorted(groups, key=lambda item: item[1])
+            elif strategy == ProtectedGroupStrategy.MAJORITY:
+                groups = sorted(groups, key=lambda item: item[1], reverse=True)
+            else:
+                raise ValueError(
+                    f"Unknown protected group strategy: {strategy}"
+                )
+            if len(groups) > 1 and groups[0][1] == groups[1][1]:
+                # Tie in group selection.
+                count = groups[0][1]
+                tie_groups = [
+                    group[0]
+                    for group in groups
+                    if group[1] == count
+                ]
+                tie_breaking = self._tie_breaking_param
+                if tie_breaking is None:
+                    raise ValueError(
+                        f"Could not select protected group "
+                        f"by {strategy.value} because of a tie. "
+                        f"Groups {tie_groups} all occur {count} time(s)."
+                    )
+                elif isinstance(tie_breaking, TieBreakingStrategy):
+                    if not all(hasattr(g, "__lt__") for g in tie_groups):
+                        raise ValueError(
+                            f"Tie breaking {tie_breaking.value} requires "
+                            f"sorting but groups are not "
+                            f"sortable: {tie_groups}"
+                        )
+                    if tie_breaking == TieBreakingStrategy.RANDOM:
+                        return choice(tie_groups)
+                    elif tie_breaking == TieBreakingStrategy.GROUP_ASCENDING:
+                        # noinspection PyTypeChecker
+                        return sorted(tie_groups)[0]
+                    elif tie_breaking == TieBreakingStrategy.GROUP_DESCENDING:
+                        # noinspection PyTypeChecker
+                        return sorted(tie_groups, reverse=True)[0]
+                else:
+                    tie_breaking_groups = [
+                        group
+                        for group in tie_breaking
+                        if group in tie_groups
+                    ]
+                    if len(tie_breaking_groups) == 0:
+                        raise ValueError(
+                            f"Tie breaking preference {tie_breaking} not "
+                            f"applicable to resolve tie: {tie_groups}"
+                        )
+                    return tie_breaking_groups[0]
+            return groups[0][0]
+        else:
+            return protected_group
+
     def _compute_query(
             self,
             qrels: DataFrame,
             ranking: DataFrame,
             groups: set[Hashable],
     ) -> float:
-        group_counts = self._group_counts(qrels, groups)
+        group_counts = self._group_counts(qrels)
         protected_group = self._protected_group(group_counts)
         if protected_group not in groups:
             raise ValueError(
@@ -115,32 +226,6 @@ class FairnessMeasure(Measure, ABC):
             return self._groups_param
         return set(qrels[self._group_col_param].unique().tolist())
 
-    def _group_counts(
-            self,
-            qrels: DataFrame,
-            groups: set[Hashable],
-    ) -> dict[Hashable, int]:
-        counts = qrels.groupby(self._group_col_param).size().to_dict()
-        return {
-            group: counts.get(group, 0)
-            for group in groups
-        }
-
-    def _protected_group(self, group_counts: dict[Hashable, int]) -> Hashable:
-        protected_group: Hashable = self._protected_group_param
-        if isinstance(protected_group, ProtectedGroupSelection):
-            groups: list[tuple[Hashable, int]] = [
-                item for item in group_counts.items()
-            ]
-            if protected_group == ProtectedGroupSelection.MINORITY:
-                return sorted(groups, key=lambda item: item[1])[0][0]
-            elif protected_group == ProtectedGroupSelection.MAJORITY:
-                return sorted(groups, key=lambda item: item[1])[-1][0]
-            else:
-                raise ValueError()
-        else:
-            return protected_group
-
     def compute(self, qrels: DataFrame, run: DataFrame) -> Iterator[Metric]:
         groups = self._groups(qrels)
 
@@ -151,29 +236,43 @@ class FairnessMeasure(Measure, ABC):
                 drop=True)
 
         for qid, ranking in run.groupby("query_id"):
-            qid: str
-            ranking_qrels = qrels[qrels["query_id"] == qid]
             yield Metric(
-                qid,
+                str(qid),
                 self,
-                self._compute_query(ranking_qrels, ranking, groups)
+                self._compute_query(
+                    qrels[qrels["query_id"] == qid],
+                    ranking,
+                    groups,
+                )
             )
 
     def __str__(self):
         name = self.NAME
-        cutoff = (
-            f"@{self._cutoff_param}"
-            if self._cutoff_param is not None
-            else ""
-        )
-        protected_group = (
-            self._protected_group_param.value
-            if isinstance(self._protected_group_param, ProtectedGroupSelection)
-            else repr(self._protected_group_param)
-        )
+        cutoff = ""
+        if self._cutoff_param is not None:
+            cutoff = f"@{self._cutoff_param}"
+        group_col = None
+        if self._group_col_param != "group":
+            group_col = repr(self._group_col_param)
+        protected_group = None
+        if isinstance(self._protected_group_param, ProtectedGroupStrategy):
+            if self._protected_group_param != ProtectedGroupStrategy.MINORITY:
+                protected_group = repr(self._protected_group_param.value)
+        elif isinstance(self._protected_group_param, Hashable):
+            protected_group = repr(self._protected_group_param)
+        tie_breaking = None
+        if isinstance(self._tie_breaking_param, TieBreakingStrategy):
+            tie_breaking = repr(self._tie_breaking_param.value)
+        elif isinstance(self._tie_breaking_param, Sequence):
+            tie_breaking = repr(",".join(self._tie_breaking_param))
         params = [
-            param
-            for param in (self._groups_param, protected_group)
+            f"{name}={param}"
+            for name, param in {
+                "group_col": group_col,
+                "groups": self._groups_param,
+                "protected_group": protected_group,
+                "tie_breaking": tie_breaking,
+            }.items()
             if param is not None
         ]
         return f"{name}{cutoff}({','.join(params)})"
