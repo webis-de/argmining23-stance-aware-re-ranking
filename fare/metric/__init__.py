91439b03b6b44823013b02a7341cd91143019163
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-12 23:32:42 +0100

    Fix reading measures

--- fare/metric/__init__.py
+++ fare/metric/__init__.py
@@ -1,9 +1,10 @@
 from ir_measures import Measure, parse_measure as _parse_measure
+from ir_measures.measures import registry
 
-from fare.metric.f1 import F1
+from fare.metric.f1 import F1, F1_Touche
 
 
 def parse_measure(measure: str) -> Measure:
-    if measure.split("@")[0] == "F1":
-        return F1()
+    for additional_measure in (F1(), F1_Touche()):
+        registry[additional_measure.NAME] = additional_measure
     return _parse_measure(measure)
