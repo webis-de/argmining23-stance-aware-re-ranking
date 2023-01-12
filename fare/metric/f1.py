5fbcff7fc673a4418d97f036aa5c78b224468f5e
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-12 23:32:24 +0100

    Add Touché F1 score

--- fare/metric/f1.py
+++ fare/metric/f1.py
@@ -8,21 +8,40 @@ from sklearn.metrics import f1_score
 def _f1_score(
         qrels: DataFrame,
         run: DataFrame,
+        across_all_queries: bool = False
 ) -> Iterable[Tuple[str, float]]:
     df = merge(
         qrels,
         run,
         on=["query_id", "doc_id"],
-        how="right",
+        how="left",
         suffixes=("_qrels", "_run"),
     )
-    df = df[~df["stance_label_qrels"].isna() & ~df["stance_label_run"].isna()]
-    for qid, df in df.groupby("query_id", sort=False):
-        yield qid, f1_score(
+    df = df.dropna(subset=["stance_label_qrels", "stance_label_run"])
+    if across_all_queries:
+        score = f1_score(
             df["stance_label_qrels"],
             df["stance_label_run"],
             average="macro",
         )
+        for qid in df["query_id"].unique():
+            yield qid, score
+    else:
+        for qid, df in df.groupby("query_id", sort=False):
+            yield qid, f1_score(
+                df["stance_label_qrels"],
+                df["stance_label_run"],
+                average="macro",
+            )
 
 
-F1 = define(_f1_score, name="F1")
+def _f1_score_touche(
+        qrels: DataFrame,
+        run: DataFrame,
+) -> Iterable[Tuple[str, float]]:
+    return _f1_score(qrels, run, across_all_queries=True)
+
+
+F1 = define(_f1_score, name="F1", support_cutoff=True)
+
+F1_Touche = define(_f1_score_touche, name="F1_Touche", support_cutoff=True)
