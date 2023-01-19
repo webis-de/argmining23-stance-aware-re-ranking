78973bcf8658f20103673825f4c30c52c322bda6
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-19 18:25:14 +0100

    Update classification measures

--- fare/metric/__init__.py
+++ fare/metric/__init__.py
@@ -2,15 +2,14 @@ from ir_measures import Measure, parse_measure as _parse_measure
 from ir_measures.measures import registry
 
 from fare.metric.classification import (
-    F1, F1All, Confidence, PropFirst, PropSecond, PropNeutral, PropNo,
-    JudgedAll
+    F1, NumJudged, FreqFirst, FreqSecond, FreqNeutral, FreqNo
 )
 
 
 def parse_measure(measure: str) -> Measure:
     for additional_measure in (
-            F1(), F1All(), Confidence(), PropFirst(), PropSecond(),
-            PropNeutral(), PropNo(), JudgedAll()
+            F1(), NumJudged(), FreqFirst(), FreqSecond(), FreqNeutral(),
+            FreqNo(),
     ):
         registry[additional_measure.NAME] = additional_measure
     return _parse_measure(measure)
--- fare/metric/classification.py
+++ fare/metric/classification.py
@@ -1,4 +1,3 @@
-from math import nan
 from typing import Tuple, Iterable, Callable
 
 from ir_measures import define
@@ -9,7 +8,6 @@ from sklearn.metrics import f1_score
 def _f1_score(
         qrels: DataFrame,
         run: DataFrame,
-        across_all_queries: bool = False
 ) -> Iterable[Tuple[str, float]]:
     df = qrels.merge(
         run,
@@ -18,33 +16,16 @@ def _f1_score(
         suffixes=("_qrels", "_run"),
     )
     df = df.dropna(subset=["stance_label_qrels", "stance_label_run"])
-    if across_all_queries:
-        score = f1_score(
-            df["stance_label_qrels"],
-            df["stance_label_run"],
-            average="macro",
-        )
-        for qid in df["query_id"].unique():
-            yield qid, score
-    else:
-        for qid, df in df.groupby("query_id", sort=False):
-            yield qid, f1_score(
-                df["stance_label_qrels"],
-                df["stance_label_run"],
-                average="macro",
-            )
-
-
-def _f1_score_touche(
-        qrels: DataFrame,
-        run: DataFrame,
-) -> Iterable[Tuple[str, float]]:
-    return _f1_score(qrels, run, across_all_queries=True)
+    score = f1_score(
+        df["stance_label_qrels"],
+        df["stance_label_run"],
+        average="macro",
+    )
+    for qid in df["query_id"].unique():
+        yield qid, score
 
 
 F1 = define(_f1_score, name="F1", support_cutoff=True)
-
-F1All = define(_f1_score_touche, name="F1All", support_cutoff=True)
 
 
 def _judged_stance(
@@ -62,42 +43,26 @@ def _judged_stance(
         yield qid, judged
 
 
-JudgedAll = define(_judged_stance, name="JudgedAll", support_cutoff=True)
+NumJudged = define(_judged_stance, name="NumJudged", support_cutoff=True)
 
 
-def _confidence(
-        _: DataFrame,
-        run: DataFrame,
-) -> Iterable[Tuple[str, float]]:
-    for qid, df in run.groupby("query_id", sort=False):
-        stance = df["stance_value"] \
-            .replace(0, nan) \
-            .dropna() \
-            .abs()
-        yield qid, stance.mean()
-
-
-Confidence = define(_confidence, name="Confidence", support_cutoff=True)
-
-
-def _proportion(
+def _frequency(
         label: str
 ) -> Callable[[DataFrame, DataFrame], Iterable[Tuple[str, float]]]:
     def _wrapped(
             _: DataFrame,
             run: DataFrame,
     ) -> Iterable[Tuple[str, float]]:
-        for qid, df in run.groupby("query_id", sort=False):
-            df = df.dropna(subset=["stance_label"])
-            proportion = len(df[df["stance_label"] == label]) / len(df)
-            yield qid, proportion
+        frequency = len(run[run["stance_label"] == label]) / len(run)
+        for qid in run["query_id"].unique():
+            yield qid, frequency
 
     return _wrapped
 
 
-PropFirst = define(_proportion("FIRST"), name="PropFirst", support_cutoff=True)
-PropSecond = define(_proportion("SECOND"), name="PropSecond",
+FreqFirst = define(_frequency("FIRST"), name="FreqFirst", support_cutoff=True)
+FreqSecond = define(_frequency("SECOND"), name="FreqSecond",
                     support_cutoff=True)
-PropNeutral = define(_proportion("NEUTRAL"), name="PropNeutral",
+FreqNeutral = define(_frequency("NEUTRAL"), name="FreqNeutral",
                      support_cutoff=True)
-PropNo = define(_proportion("NO"), name="PropNo", support_cutoff=True)
+FreqNo = define(_frequency("NO"), name="FreqNo", support_cutoff=True)
