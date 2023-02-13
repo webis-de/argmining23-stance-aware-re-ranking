de3b7c5d917603463c2d45cf414b1d82dfb795e2
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-02-13 18:25:22 +0100

    Add diversity reranker

--- fare/modules/diversity_reranker.py
+++ fare/modules/diversity_reranker.py
@@ -8,12 +8,10 @@ from numpy import arange
 from pandas import DataFrame, Series
 from pyterrier import Transformer
 from pyterrier.model import add_ranks
-from tqdm.auto import tqdm
 
 
 @dataclass(frozen=True)
-class AlternatingStanceDiversityReranker(Transformer):
-    verbose: bool = False
+class StanceStreakDiversityReranker(Transformer):
 
     @staticmethod
     def _transform_query(ranking: DataFrame) -> DataFrame:
@@ -54,24 +52,73 @@ class AlternatingStanceDiversityReranker(Transformer):
 
     def transform(self, ranking: DataFrame) -> DataFrame:
         groups = ranking.groupby("qid", sort=False, group_keys=False)
-        if self.verbose:
-            tqdm.pandas(desc="Rerank alternating stance", unit="query")
-            groups = groups.progress_apply(self._transform_query)
-        else:
-            groups = groups.apply(self._transform_query)
-        ranking = groups.reset_index(drop=True)
+        ranking = groups.apply(self._transform_query)
+        ranking = ranking.reset_index(drop=True)
+        ranking = add_ranks(ranking)
+        return ranking
+
+
+@dataclass(frozen=True)
+class AlternatingStanceDiversityReranker(Transformer):
+    revision: int = 2
+
+    @staticmethod
+    def _transform_query(ranking: DataFrame) -> DataFrame:
+        new_rows: List[Series] = []
+        last_stance: float = nan
+        while len(ranking) > 0:
+            candidates: DataFrame
+            if last_stance > 0:
+                # Last document was pro A.
+                # Find first pro B or neutral document next.
+                candidates = ranking[ranking["stance_value"] <= 0]
+            elif last_stance < 0:
+                # Last document was pro B.
+                # Find first pro A or neutral document next.
+                candidates = ranking[ranking["stance_value"] >= 0]
+            else:
+                # Last document was neutral.
+                # Find any document next, regardless of stance.
+                candidates = ranking
+
+            if len(candidates) == 0:
+                # No candidate for the stance was found,
+                # choose any document next,
+                # regardless of stance.
+                last_stance = nan
+                continue
+
+            index = candidates.index.tolist()[0]
+            document: Series = candidates.iloc[0]
+            if document["stance_value"] > 0 or document["stance_value"] < 0:
+                last_stance = document["stance_value"]
+            new_rows.append(document)
+            ranking.drop(index=index, inplace=True)
+        ranking = DataFrame(data=new_rows, columns=ranking.columns)
+
+        # Reset score.
+        ranking["score"] = arange(len(ranking), 0, -1)
+        return ranking
+
+    def transform(self, ranking: DataFrame) -> DataFrame:
+        groups = ranking.groupby("qid", sort=False, group_keys=False)
+        ranking = groups.apply(self._transform_query)
+        ranking = ranking.reset_index(drop=True)
         ranking = add_ranks(ranking)
         return ranking
 
 
 class DiversityReranker(Transformer, Enum):
     ORIGINAL = "original"
+    STANCE_STREAK = "stance-streak"
     ALTERNATING_STANCE = "alternating-stance"
 
     @cached_property
     def _transformer(self) -> Transformer:
         if self == DiversityReranker.ORIGINAL:
             return Transformer.identity()
+        elif self == DiversityReranker.STANCE_STREAK:
+            return StanceStreakDiversityReranker()
         elif self == DiversityReranker.ALTERNATING_STANCE:
             return AlternatingStanceDiversityReranker()
         else:
