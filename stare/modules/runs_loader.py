58fc3e102ccd5678f38c8dda94f5bf3091a2fce1
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-09-07 22:53:15 +0200

    Optimize run loader

--- stare/modules/runs_loader.py
+++ stare/modules/runs_loader.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from functools import cached_property
+from functools import cached_property, cache
 from pathlib import Path
 
 from pandas import DataFrame, read_csv
@@ -9,6 +9,26 @@ from pyterrier.transformer import Transformer
 from stare.utils.stance import stance_value
 
 
+@cache
+def _read_run(run_file_path: Path) -> DataFrame:
+    ranking = read_csv(
+        str(run_file_path.absolute()),
+        sep="\\s+",
+        names=["qid", "stance_label", "docno", "rank", "score", "name"],
+    )
+    ranking = ranking.astype({"qid": "str"})
+    ranking["stance_label"] = ranking["stance_label"].replace("Q0", "NO")
+    ranking["stance_value"] = ranking["stance_label"].map(stance_value)
+    ranking.sort_values(
+        ["qid", "rank"],
+        ascending=[True, True],
+        inplace=True,
+    )
+    ranking = add_ranks(ranking)
+    return ranking
+
+
+
 @dataclass(frozen=True)
 class RunLoader(Transformer):
     run_file_path: Path
@@ -16,21 +36,7 @@ class RunLoader(Transformer):
 
     @cached_property
     def _ranking(self) -> DataFrame:
-        ranking = read_csv(
-            str(self.run_file_path.absolute()),
-            sep="\\s+",
-            names=["qid", "stance_label", "docno", "rank", "score", "name"],
-        )
-        ranking = ranking.astype({"qid": "str"})
-        ranking["stance_label"] = ranking["stance_label"].replace("Q0", "NO")
-        ranking["stance_value"] = ranking["stance_label"].map(stance_value)
-        ranking.sort_values(
-            ["qid", "rank"],
-            ascending=[True, True],
-            inplace=True,
-        )
-        ranking = add_ranks(ranking)
-        return ranking
+        return _read_run(self.run_file_path)
 
     @cached_property
     def name(self) -> str:
